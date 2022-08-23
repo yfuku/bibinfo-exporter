01b0828b10d21b88d695324ae4b02099b9f31bad
yfuku <fukumoto@gmail.com>
2022-08-23 10:08:53 +0900

    jsの配信方法をgithub,jsdelivrを使用するように変更

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # bibinfo-exporter
 Bookmarklet for export Amazon Bibliographic information
 
-## 使い方
+# js配信
+jsdelivrを使ってみた
+https://www.jsdelivr.com/
+
+キャッシュを削除するには↓のURLを叩く
+https://purge.jsdelivr.net/gh/yfuku/bibinfo-exporter@main/script.js
+
+参考：https://rs-techdev.com/archives/4318
 
--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript: (function (d, s) { s = d.createElement('script'); s.src = 'https://scrapbox.io/api/code/your-project-name/your-page-title/script.js'; d.body.appendChild(s); })(document)
+javascript: (function (d, s) { s = d.createElement('script'); s.src = 'https://cdn.jsdelivr.net/gh/yfuku/bibinfo-exporter@main/script.js'; d.body.appendChild(s); })(document)
